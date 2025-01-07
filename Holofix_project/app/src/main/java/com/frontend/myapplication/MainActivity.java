43c69e6c10622994c687e64f297bf9b9b9db114b
Shiva-2070 <119473288+Shiva-2070@users.noreply.github.com>
2025-01-07 22:48:19 +0530

    Add files via upload

---
+++ Holofix_project/app/src/main/java/com/frontend/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package com.frontend.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    private Button buttonstart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView=findViewById(R.id.video);
+        MediaController mediaController=new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.holovid));
+
+        buttonstart=(Button)findViewById(R.id.buttonstart);
+        buttonstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2()
+    {
+        Intent intent=new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+}
